e5ac51699b1dc2ba8d6ae1fa5eb54fda82ed4c15
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2023-04-28 12:08:12 +0300

    feat: change title color

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const body = document.querySelector('body');
 // title
 const title = document.createElement('h1');
 title.textContent = 'Virtual Keyboard';
-title.style.color = '#2515ab';
+title.style.color = '#2a65ab';
 
 body.append(title);
